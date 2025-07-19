d8b74cb3d139b7fdfa19d886e1e091d8ca4ce729
Arslan Iftikhar <arsalmalik1920@gmail.com>
2025-07-19 05:53:29 -0800

    --

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,10 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'saved/:token',
+    renderMode: RenderMode.Server, // 👈 dynamic because token values are unknown at build time
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }

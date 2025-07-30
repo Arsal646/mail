fa2ca77b332656553d1335fa53940b045a3aab14
Arslan Malik <arsalmalik1920@gmail.com>
2025-07-31 00:03:53 +0400

    re-structure and re-write home content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import { Main } from './pages/main/main';
 import { SavedEmailComponent } from './pages/saved-email/saved-email.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 import { Email10MinComponent } from './pages/email-10min/email-10min.component';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
   { 
     path: '', 
-    component: Main,
+    component: Home,
     pathMatch: 'full' 
   },
   { 

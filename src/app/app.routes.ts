dc32e086d37cf0993589445ebe871153e1f6360b
Arslan Malik <66529678+Arsal646@users.noreply.github.com>
2025-08-23 22:13:05 +0400

    feat: start manual Arabic translation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SavedEmailComponent } from './pages/saved-email/saved-email.component';
 import { PrivacyPolicyComponent } from './pages/privacy-policy/privacy-policy.component';
 import { Email10MinComponent } from './pages/email-10min/email-10min.component';
 import { Home } from './pages/home/home';
+import { HomeAr } from './ar/home/home';
 import { Email20MinComponent } from './pages/email-20min/email-20min.component';
 import { Email30MinComponent } from './pages/email-30min/email-30min.component';
 
@@ -11,6 +12,11 @@ export const routes: Routes = [
   {
     path: '',
     component: Home,
+    pathMatch: 'full'
+  },
+  {
+    path: 'ar',
+    component: HomeAr,
     pathMatch: 'full'
   },
   {

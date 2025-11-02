8d233bbef2f748a60179b27a8acb896be7169d1b
Arslan Malik <arsalmalik1920@gmail.com>
2025-11-03 00:18:40 +0400

    --

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,12 +1,25 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { firstValueFrom } from 'rxjs';
+
+import { BlogService } from './services/blog.service';
+
+const blogService = new BlogService();
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'saved/:token',
-    renderMode: RenderMode.Server, // 👈 dynamic because token values are unknown at build time
+    renderMode: RenderMode.Server, // dynamic because token values are unknown at build time
+  },
+  {
+    path: 'blog/:slug',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      const posts = await firstValueFrom(blogService.getAllPosts());
+      return posts.map(post => ({ slug: post.slug }));
+    },
   },
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
